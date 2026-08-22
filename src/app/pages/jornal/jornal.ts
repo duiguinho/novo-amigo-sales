18d9292b4b13dcd54875ffddb61d1abafd95dc43
duiguinho <rodriguinhomoraes2019@gmail.com>
2026-08-22 00:16:23 -0300

    página inicial parcial

--- src/app/pages/jornal/jornal.ts
+++ src/app/pages/jornal/jornal.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-jornal',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './jornal.html',
   styleUrl: './jornal.css',
 })
--- src/app/pages/pagina-inicial/pagina-inicial.ts
+++ src/app/pages/pagina-inicial/pagina-inicial.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pagina-inicial',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './pagina-inicial.html',
   styleUrl: './pagina-inicial.css',
 })
