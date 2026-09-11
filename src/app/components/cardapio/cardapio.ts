40ac865899083c96528e489fe988846220d1401f
duiguinho <rodriguinhomoraes2019@gmail.com>
2026-09-10 23:48:33 -0300

    avisos concluido

---
+++ src/app/components/cardapio/cardapio.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cardapio',
+  imports: [],
+  templateUrl: './cardapio.html',
+  styleUrl: './cardapio.css',
+})
+export class Cardapio {}
--- src/app/pages/pagina-inicial/pagina-inicial.ts
+++ src/app/pages/pagina-inicial/pagina-inicial.ts
@@ -12,22 +12,10 @@ export class PaginaInicial {
 
 
 noticias = [
-  {
-    titulo: 'Reunião de pais',
-    descricao: 'A reunião acontecerá na próxima semana.'
-  },
-  {
-    titulo: 'Semana Paulo Freire',
-    descricao: 'Confira as atividades da semana.'
-  },
-  {
-    titulo: 'Novo aviso',
-    descricao: 'Confira as informações no mural.'
-  },
-  {
-    titulo: 'Biblioteca',
-    descricao: 'Novos livros estão disponíveis.'
-  }
+  {id:1, titulo: 'Reunião de pais', descricao: 'A reunião acontecerá na próxima semana.'},
+  {id:2, titulo: 'Semana Paulo Freire', descricao: 'Confira as atividades da semana.'},
+  {id:3, titulo: 'Novo aviso', descricao: 'Confira as informações no mural.'},
+  {id:4, titulo: 'Biblioteca', descricao: 'Novos livros estão disponíveis.'}
 ];
 
 }
